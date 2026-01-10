fecb07ec34feb3389b4410de4675cd5802d3ffee
Samuel Giraldo Concha <samuelgiraldoconcha@Samuels-MacBook-Pro.local>
2026-01-10 01:07:04 -0500

    .

--- packages/payments/index.ts
+++ packages/payments/index.ts
@@ -2,7 +2,7 @@ import "server-only";
 import Stripe from "stripe";
 import { keys } from "./keys";
 
-export const stripe = new Stripe(keys().STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(keys().STRIPE_SECRET_KEY || "", {
   apiVersion: "2025-11-17.clover",
 });
 
