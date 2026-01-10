7102b517deac9b95b99574813d48fd105f118091
Samuel Giraldo Concha <samuelgiraldoconcha@Samuels-MacBook-Pro.local>
2026-01-10 00:24:50 -0500

    .

--- packages/analytics/instrumentation-client.ts
+++ packages/analytics/instrumentation-client.ts
@@ -2,11 +2,8 @@ import posthog from "posthog-js";
 import { keys } from "./keys";
 
 export const initializeAnalytics = () => {
-  const envKeys = keys();
-  if (envKeys.NEXT_PUBLIC_POSTHOG_KEY && envKeys.NEXT_PUBLIC_POSTHOG_HOST) {
-    posthog.init(envKeys.NEXT_PUBLIC_POSTHOG_KEY, {
-      api_host: envKeys.NEXT_PUBLIC_POSTHOG_HOST,
-      defaults: "2025-05-24",
-    });
-  }
+  posthog.init(keys().NEXT_PUBLIC_POSTHOG_KEY || "", {
+    api_host: keys().NEXT_PUBLIC_POSTHOG_HOST || "",
+    defaults: "2025-05-24",
+  });
 };
